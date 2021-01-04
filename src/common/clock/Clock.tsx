a5dc2fc98c983d161016c48d6df5c6d8d6701a53
DimaZharikov <DimaZharikov@users.noreply.github.com>
2021-01-04 19:29:04 +0300

    add component Clock

---
+++ src/common/clock/Clock.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react'
+
+
+interface Props {
+
+}
+
+const getDigitsString = (num: number) => num < 10 ? '0' + num : num
+
+const ClockComponent: React.FunctionComponent<Props> = () => {
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setDate(new Date())
+        }, 1000);
+        return () => {
+            clearInterval(intervalID)
+        }
+    }, [])
+    
+
+    return <div>
+        <span>{getDigitsString(date.getHours())}</span>
+        :
+        <span>{getDigitsString(date.getMinutes())}</span>
+        :
+        <span>{getDigitsString(date.getSeconds())}</span>
+    </div>
+
+}
+
+export default ClockComponent
