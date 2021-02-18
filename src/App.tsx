c92e5dddfede4395d44c9e40ac94e3a3114fe338
DimaZharikov <DimaZharikov@users.noreply.github.com>
2021-02-18 17:24:02 +0300

    Changed Api to small classes component

--- src/App.tsx
+++ src/App.tsx
@@ -72,20 +72,14 @@ class App extends Component<MapPropsType & DispatchPropsType> {
                                 <SubMenu key="sub1" icon={<UserOutlined/>} title="My Profile">
                                     <Menu.Item key="1"> <Link to="/profile">Profile</Link></Menu.Item>
                                     <Menu.Item key="2"> <Link to="/dialogs">Messages</Link></Menu.Item>
-                                    <Menu.Item key="3">option3</Menu.Item>
-                                    <Menu.Item key="4">option4</Menu.Item>
+
                                 </SubMenu>
                                 <SubMenu key="sub2" icon={<LaptopOutlined/>} title="Developers">
                                     <Menu.Item key="5"><Link to="/developers">Developers</Link></Menu.Item>
-                                    <Menu.Item key="6">option6</Menu.Item>
-                                    <Menu.Item key="7">option7</Menu.Item>
-                                    <Menu.Item key="8">option8</Menu.Item>
+
                                 </SubMenu>
                                 <SubMenu key="sub3" icon={<NotificationOutlined/>} title="subnav 3">
                                     <Menu.Item key="9"><Link to="/chat">Chat</Link></Menu.Item>
-                                    <Menu.Item key="10">option10</Menu.Item>
-                                    <Menu.Item key="11">option11</Menu.Item>
-                                    <Menu.Item key="12">option12</Menu.Item>
                                 </SubMenu>
                             </Menu>
                         </Sider>
@@ -117,7 +111,7 @@ class App extends Component<MapPropsType & DispatchPropsType> {
                         </Content>
                     </Layout>
                 </Content>
-                <Footer style={{textAlign: 'center'}}>Samurai Social Network ©2020 Created by IT-KAMASUTRA</Footer>
+
             </Layout>
 
 
--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,7 @@ export const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers:     {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "53463a8d-8572-4c71-942e-6aa342aad633"
     }
 });
 
